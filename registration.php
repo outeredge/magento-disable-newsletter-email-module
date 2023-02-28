1ca8a96bd725805ed37595717d9f58a3b55c64a2
Victor Seager <vseager@gmail.com>
2023-02-28 13:01:46 +0000

    Add phpcs

--- registration.php
+++ registration.php
@@ -1,6 +1,8 @@
 <?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'OuterEdge_NewsletterDisableEmail',
-    __DIR__
-);
+if (class_exists('Magento\Framework\Component\ComponentRegistrar')) {
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'OuterEdge_NewsletterDisableEmail',
+        __DIR__
+    );
+}
